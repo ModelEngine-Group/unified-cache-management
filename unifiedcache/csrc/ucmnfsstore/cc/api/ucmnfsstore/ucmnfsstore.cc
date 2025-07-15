3e46537ed3db6be7187ea7f9a76ae1e147d0836e
Mag1c.H <hemajun815@163.com>
2025-07-15 19:15:13 +0800

    nfsstore interface

---
+++ unifiedcache/csrc/ucmnfsstore/cc/api/ucmnfsstore/ucmnfsstore.cc
@@ -0,0 +1,57 @@
+/**
+/* MIT License
+/*
+/* Copyright (c) 2025 Huawei Technologies Co., Ltd. All rights reserved.
+/*
+/* Permission is hereby granted, free of charge, to any person obtaining a copy
+/* of this software and associated documentation files (the "Software"), to deal
+/* in the Software without restriction, including without limitation the rights
+/* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/* copies of the Software, and to permit persons to whom the Software is
+/* furnished to do so, subject to the following conditions:
+/*
+/* The above copyright notice and this permission notice shall be included in all
+/* copies or substantial portions of the Software.
+/*
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+/* SOFTWARE.
+ * */
+#include "ucmnfsstore.h"
+
+namespace UC {
+
+int32_t Setup(const SetupParam& param)
+{
+    return 0;
+}
+
+int32_t Alloc(const std::string& blockId)
+{
+    return 0;
+}
+
+bool Lookup(const std::string& blockId)
+{
+    return false;
+}
+
+size_t Submit(std::list<TsfTask> tasks)
+{
+    return size_t();
+}
+
+int32_t Wait(const size_t taskId)
+{
+    return 0;
+}
+
+void Commit(const std::string& blockId, const bool success)
+{
+}
+
+} // namespace UC
