04e22f9690e4a1fe82be0f23dd3e0179754334ee
propanone1006 <1035067916@qq.com>
2025-07-22 11:53:18 +0800

    fix bugs

--- unifiedcache/csrc/ucmnfsstore/cc/domain/tsf_task/tsf_task_runner.h
+++ unifiedcache/csrc/ucmnfsstore/cc/domain/tsf_task/tsf_task_runner.h
@@ -1,25 +1,25 @@
 /**
-/* MIT License
-/*
-/* Copyright (c) 2025 Huawei Technologies Co., Ltd. All rights reserved.
-/*
-/* Permission is hereby granted, free of charge, to any person obtaining a copy
-/* of this software and associated documentation files (the "Software"), to deal
-/* in the Software without restriction, including without limitation the rights
-/* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/* copies of the Software, and to permit persons to whom the Software is
-/* furnished to do so, subject to the following conditions:
-/*
-/* The above copyright notice and this permission notice shall be included in all
-/* copies or substantial portions of the Software.
-/*
-/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-/* SOFTWARE.
+* MIT License
+*
+* Copyright (c) 2025 Huawei Technologies Co., Ltd. All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
  * */
 #ifndef UNIFIEDCACHE_TSF_TASK_RUNNER
 #define UNIFIEDCACHE_TSF_TASK_RUNNER
@@ -36,8 +36,8 @@ public:
     Status Run(const TsfTask& task, IDevice* device);
      
 private:
-    Status Host2SSD(const TsfTask& task);   // todo
-    Status SSD2Host(const TsfTask& task);   // todo
+    Status Host2SSD(const TsfTask& task);   
+    Status SSD2Host(const TsfTask& task);   
     Status Device2SSD(const TsfTask& task); // todo
     Status SSD2Device(const TsfTask& task); // todo
     Status ReadFile(const TsfTask& task, bool align = false);
