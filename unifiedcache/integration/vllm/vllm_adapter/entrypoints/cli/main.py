8466f0959d1dcc728f975a6699f4344791934bfb
Chenyu Zhu <zhuchenyu1@h-partners.com>
2025-09-12 15:23:02 +0800

    fix

--- unifiedcache/integration/vllm/vllm_adapter/entrypoints/cli/main.py
+++ unifiedcache/integration/vllm/vllm_adapter/entrypoints/cli/main.py
@@ -3,5 +3,5 @@ from vllm.entrypoints.cli.main import main as vllm_main
 import vllm
 from unifiedcache.integration.vllm import vllm_adapter
 
-if __name__ == "main":
+if __name__ == "__main__":
     vllm_main()
