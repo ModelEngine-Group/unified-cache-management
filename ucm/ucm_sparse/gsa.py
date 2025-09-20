08a14d506ea86ba8de943dabf07bd31d4bed5a02
HaoLi980405 <hl1390208045@gmail.com>
2025-09-20 20:36:22 +0800

    ci

--- ucm/ucm_sparse/gsa.py
+++ ucm/ucm_sparse/gsa.py
@@ -126,7 +126,7 @@ class GSAReqStat:
         )
         self._init_slot(offset)
         if len(self.repre_slot_mapping) > len(self.blocks):
-            self.repre_slot_mapping = self.repre_slot_mapping[:len(self.blocks)]
+            self.repre_slot_mapping = self.repre_slot_mapping[: len(self.blocks)]
 
     def updata_req_state(
         self, num_scheduled_tokens, add_req_state, index_in_batch
@@ -150,7 +150,7 @@ class GSAReqStat:
                 self.calc_block_table = []
                 self.calc_repre_slot_mapping = []
         if len(self.repre_slot_mapping) > len(self.blocks):
-            self.repre_slot_mapping = self.repre_slot_mapping[:len(self.blocks)]
+            self.repre_slot_mapping = self.repre_slot_mapping[: len(self.blocks)]
 
     def _get_sparse_and_free_block(self):
         if self.num_prompt_tokens == self.num_computed_tokens:
