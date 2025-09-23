3fa2d033145083ea5b0308dd90187bad6d00ace1
root <fenghao78@huawei.com>
2025-09-22 19:53:06 -0700

    kv_block_size as well as transferIoSize are calculated rather than configured

--- ucm/integration/vllm/uc_connector.py
+++ ucm/integration/vllm/uc_connector.py
@@ -136,12 +136,13 @@ class UnifiedCacheConnectorV1(KVConnectorBase_V1):
                 "scheduler" if role == KVConnectorRole.SCHEDULER else "worker"
             )
             config_base = self.block_size * self.element_size * self.head_size
-            config["kv_block_size"]  = (
-                config_base * self.num_layers 
+            config["kv_block_size"] = (
+                config_base
+                * self.num_layers
                 * (1 if self.is_mla else self.num_head * self.total_tp_size * 2)
             )
-            config["transferIoSize"] = (
-                config_base * (1 if self.is_mla else self.num_head)
+            config["transferIoSize"] = config_base * (
+                1 if self.is_mla else self.num_head
             )
             logger.info(
                 "kv_block_size = %d, transferIoSize = %d,",
