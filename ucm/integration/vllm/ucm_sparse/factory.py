10803947e21ff858acfd67410b3a0e74ab157752
Chenyu Zhu <zhuchenyu1@h-partners.com>
2025-09-29 09:42:46 +0800

    format

--- ucm/integration/vllm/ucm_sparse/factory.py
+++ ucm/integration/vllm/ucm_sparse/factory.py
@@ -49,4 +49,6 @@ UcmSparseFactory.register_sparse_method(
     "KvComp", "ucm.sandbox.sparse.kvcomp.kvcomp", "KvComp"
 )
 UcmSparseFactory.register_sparse_method("GSA", "ucm.ucm_sparse.gsa", "GSA")
-UcmSparseFactory.register_sparse_method("KVStarMultiStep", "ucm.ucm_sparse.kvstar.multistep", "KVStarMultiStep")
+UcmSparseFactory.register_sparse_method(
+    "KVStarMultiStep", "ucm.ucm_sparse.kvstar.multistep", "KVStarMultiStep"
+)
