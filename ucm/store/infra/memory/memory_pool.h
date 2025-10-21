1583e40217921223e9fb5acd6dcc61b4e551efac
Chenyu Zhu <zhuchenyu1@h-partners.com>
2025-10-21 14:54:34 +0800

    add memory pool

---
+++ ucm/store/infra/memory/memory_pool.h
@@ -0,0 +1,157 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * */
+#ifndef UNIFIEDCACHE_MEMORY_POOL_H
+#define UNIFIEDCACHE_MEMORY_POOL_H
+
+#include <cstddef>
+#include <cstdlib>
+#include <string>
+#include <list>
+#include <unordered_map>
+#include <set>
+#include "status/status.h"
+
+namespace UC {
+
+class MemoryPool {
+public:
+    MemoryPool(uint32_t capacity, uint32_t blockSize)
+        : pool_(new char[capacity]),
+          capacity_(capacity),
+          blockSize_(blockSize) {
+        if (!pool_) {
+            throw std::bad_alloc();
+        }
+        uint32_t slotNum = capacity / blockSize;
+        for (uint32_t i = 0; i < slotNum; ++i) {
+            // 将所有槽位都预先占好，插入LRU队列中。
+            std::string dummy = "__slot_" + std::to_string(i);
+            char* addr = pool_ + i * blockSize_;
+            lruList_.push_front(dummy);
+            lruIndex_[dummy] = lruList_.begin();
+            addressMap_[dummy] = addr;
+        }
+    }
+
+    ~MemoryPool() {
+        delete[] pool_;
+    }
+
+    MemoryPool(const MemoryPool&) = delete;
+    MemoryPool& operator=(const MemoryPool&) = delete;
+
+    Status NewBlock(const std::string& blockId) {
+        if (addressMap_.count(blockId)) {
+            return Status::DuplicateKey();
+        }
+        if (lruList_.empty()) {
+            // 所有空间里的块都正在写，那么就不能够分配
+            return Status::Error();
+        }
+        char* addr = LRUEvictOne();
+        addressMap_[blockId] = addr;
+        return Status::OK();
+    }
+
+    bool LookupBlock(const std::string& blockId) const {
+        return availableBlocks_.count(blockId);
+    }
+
+    char* GetAddress(const std::string& blockId) const {
+        auto it = addressMap_.find(blockId);
+        return it == addressMap_.end() ? nullptr : it->second;
+    }
+
+    Status CommitBlock(const std::string& blockId, bool success) {
+        if (success) {
+            availableBlocks_.insert(blockId);
+            touchUnsafe(blockId);
+        } else {
+            resetSpaceOfBlock(blockId);
+        }
+        return Status::OK();
+    }
+
+    // 单元测试用，外部应该用不到
+    char* GetFirstAddr() {
+        return pool_;
+    }
+
+private:
+    char* pool_ = nullptr;
+    uint32_t capacity_;
+    uint32_t blockSize_;
+
+    std::unordered_map<std::string, char*> addressMap_;
+    std::set<std::string> availableBlocks_;
+
+    using ListType = std::list<std::string>;
+    ListType lruList_;
+    std::unordered_map<std::string, ListType::iterator> lruIndex_;
+
+    void touchUnsafe(const std::string& blockId) {
+        auto it = lruIndex_.find(blockId);
+        if (it != lruIndex_.end()) {
+            lruList_.splice(lruList_.begin(), lruList_, it->second);
+        }
+        else {
+            lruList_.push_front(blockId); // 访问一次，该块就是最近使用了的，所以放到LRU队列的头部。这就是一般LRU的逻辑
+            lruIndex_[blockId] = lruList_.begin();
+        }
+    }
+
+    char* LRUEvictOne() {
+        const std::string& victim = lruList_.back();
+        // 真实数据块，才从availableBlocks_中删掉
+        if (victim.rfind("__slot_", 0) != 0) {
+            availableBlocks_.erase(victim);
+        }
+        char* addr = addressMap_[victim];
+        addressMap_.erase(victim);
+        lruIndex_.erase(victim);
+        lruList_.pop_back();
+        return addr;
+    }
+
+    void resetSpaceOfBlock(const std::string& blockId) {
+        // availableBlocks_.erase(blockId); // 这句大概不需要？
+        auto it = addressMap_.find(blockId);
+        char* addr = it->second;
+        int32_t offset = static_cast<uint32_t>(addr - pool_);
+        std::string dummy = "__slot_" + std::to_string(offset / blockSize_);
+        addressMap_.erase(blockId);
+
+        auto lit = lruIndex_.find(blockId);
+        if (lit != lruIndex_.end()) {
+            lruList_.erase(lit->second);
+            lruIndex_.erase(lit);
+        }
+        lruList_.push_back(dummy); // 将一个块commit false后，回收之前分配的内存，并且要将其放到LRU队列的尾部（下次可以写的时候，要马上就写。因为该块的优先级高于已经写了的块）
+        lruIndex_[dummy] = std::prev(lruList_.end());
+        addressMap_[dummy] = addr;
+    }
+};
+
+} // namespace UC
+#endif
---
+++ ucm/store/test/case/infra/mem_pool_test.cc
@@ -0,0 +1,136 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * */
+
+#include "infra/memory/memory_pool.h"
+#include <gtest/gtest.h>
+
+class UCMemoryPoolTest : public ::testing::Test {};
+
+TEST_F(UCMemoryPoolTest, NewBlockAllocateAndCommit)
+{
+    UC::MemoryPool memPool(10, 2); // 初始化内存池
+    const std::string block1 = "block1";
+    ASSERT_FALSE(memPool.LookupBlock(block1));
+    ASSERT_EQ(memPool.GetAddress(block1), nullptr);
+    ASSERT_EQ(memPool.NewBlock(block1), UC::Status::OK());
+    ASSERT_FALSE(memPool.LookupBlock(block1));
+    ASSERT_NE(memPool.GetAddress(block1), nullptr);
+    ASSERT_EQ(memPool.NewBlock(block1), UC::Status::DuplicateKey());
+    ASSERT_EQ(memPool.CommitBlock(block1, true), UC::Status::OK());
+    ASSERT_TRUE(memPool.LookupBlock(block1));
+}
+
+TEST_F(UCMemoryPoolTest, EvictOldBlock)
+{
+    UC::MemoryPool memPool(10, 5); // 初始化内存池
+    const std::string block1 = "block1";
+    const std::string block2 = "block2";
+    const std::string block3 = "block3";
+    ASSERT_EQ(memPool.NewBlock(block1), UC::Status::OK());
+    ASSERT_NE(memPool.GetAddress(block1), nullptr);
+    ASSERT_EQ(memPool.NewBlock(block2), UC::Status::OK());
+    ASSERT_NE(memPool.GetAddress(block2), nullptr);
+    memPool.CommitBlock(block1, true);
+    memPool.CommitBlock(block2, true);
+    ASSERT_EQ(memPool.NewBlock(block3), UC::Status::OK());
+    ASSERT_NE(memPool.GetAddress(block3), nullptr);
+    ASSERT_EQ(memPool.GetAddress(block1), nullptr);
+    ASSERT_NE(memPool.GetAddress(block2), nullptr);
+    ASSERT_FALSE(memPool.LookupBlock(block1));
+    ASSERT_TRUE(memPool.LookupBlock(block2));
+    ASSERT_FALSE(memPool.LookupBlock(block3));
+}
+
+TEST_F(UCMemoryPoolTest, OldBlockCommitFalse)
+{
+    UC::MemoryPool memPool(32, 8); // 初始化内存池
+    const std::string block1 = "block1";
+    const std::string block2 = "block2";
+    const std::string block3 = "block3";
+    const std::string block4 = "block4";
+    const std::string block5 = "block5";
+    ASSERT_EQ(memPool.NewBlock(block1), UC::Status::OK());
+    ASSERT_NE(memPool.GetAddress(block1), nullptr);
+    ASSERT_EQ(memPool.NewBlock(block2), UC::Status::OK());
+    ASSERT_NE(memPool.GetAddress(block2), nullptr);
+    ASSERT_EQ(memPool.NewBlock(block3), UC::Status::OK());
+    ASSERT_NE(memPool.GetAddress(block3), nullptr);
+    memPool.CommitBlock(block1, true);
+    memPool.CommitBlock(block2, false);
+    ASSERT_TRUE(memPool.LookupBlock(block1));
+    ASSERT_FALSE(memPool.LookupBlock(block2));
+    ASSERT_FALSE(memPool.LookupBlock(block3));
+    ASSERT_EQ(memPool.NewBlock(block4), UC::Status::OK());
+    ASSERT_EQ(static_cast<uint32_t>(memPool.GetAddress((block4)) - memPool.GetFirstAddr()), 8);
+    ASSERT_EQ(memPool.NewBlock(block5), UC::Status::OK());
+    ASSERT_EQ(static_cast<uint32_t>(memPool.GetAddress((block5)) - memPool.GetFirstAddr()), 24);
+    memPool.CommitBlock(block3, true);
+    memPool.CommitBlock(block4, true);
+    memPool.CommitBlock(block5, true);
+    ASSERT_TRUE(memPool.LookupBlock(block1));
+    ASSERT_FALSE(memPool.LookupBlock(block2));
+    ASSERT_TRUE(memPool.LookupBlock(block3));
+    ASSERT_TRUE(memPool.LookupBlock(block4));
+    ASSERT_TRUE(memPool.LookupBlock(block5));
+
+    ASSERT_EQ(memPool.NewBlock(block1), UC::Status::DuplicateKey());
+    ASSERT_EQ(memPool.NewBlock(block2), UC::Status::OK());
+    ASSERT_EQ(static_cast<uint32_t>(memPool.GetAddress((block2)) - memPool.GetFirstAddr()), 0);
+    ASSERT_FALSE(memPool.LookupBlock(block1));
+    ASSERT_FALSE(memPool.LookupBlock(block2));
+    memPool.CommitBlock(block2, true);
+    ASSERT_TRUE(memPool.LookupBlock(block2));
+}
+
+TEST_F(UCMemoryPoolTest, NoCommittedBlock)
+{
+    UC::MemoryPool memPool(32, 8); // 初始化内存池
+    const std::string block1 = "block1";
+    const std::string block2 = "block2";
+    const std::string block3 = "block3";
+    const std::string block4 = "block4";
+    const std::string block5 = "block5";
+    const std::string block6 = "block6";
+    ASSERT_EQ(memPool.NewBlock(block1), UC::Status::OK());
+    ASSERT_EQ(memPool.NewBlock(block2), UC::Status::OK());
+    ASSERT_EQ(memPool.NewBlock(block3), UC::Status::OK());
+    ASSERT_EQ(memPool.NewBlock(block4), UC::Status::OK());
+    ASSERT_EQ(memPool.NewBlock(block5), UC::Status::Error());
+    memPool.CommitBlock(block1, true);
+    ASSERT_TRUE(memPool.LookupBlock(block1));
+    ASSERT_EQ(memPool.NewBlock(block5), UC::Status::OK());
+    ASSERT_EQ(static_cast<uint32_t>(memPool.GetAddress((block5)) - memPool.GetFirstAddr()), 0);
+    ASSERT_FALSE(memPool.LookupBlock(block1));
+    ASSERT_EQ(memPool.NewBlock(block6), UC::Status::Error());
+    ASSERT_EQ(static_cast<uint32_t>(memPool.GetAddress((block2)) - memPool.GetFirstAddr()), 8);
+    memPool.CommitBlock(block2, false);
+    ASSERT_EQ(memPool.GetAddress((block2)), nullptr);
+    ASSERT_FALSE(memPool.LookupBlock(block1));
+    ASSERT_EQ(memPool.NewBlock(block6), UC::Status::OK());
+    ASSERT_EQ(static_cast<uint32_t>(memPool.GetAddress((block6)) - memPool.GetFirstAddr()), 8);
+    ASSERT_FALSE(memPool.LookupBlock(block6));
+    memPool.CommitBlock(block6, true);
+    ASSERT_TRUE(memPool.LookupBlock(block6));
+    ASSERT_EQ(static_cast<uint32_t>(memPool.GetAddress((block6)) - memPool.GetFirstAddr()), 8);
+}
