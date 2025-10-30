38c9ab5788ec244da21c234e5168f996bcae0667
Chenyu Zhu <zhuchenyu1@h-partners.com>
2025-10-30 10:26:26 +0800

    fix

--- ucm/store/dramstore/cc/domain/trans/dram_trans_queue.cc
+++ ucm/store/dramstore/cc/domain/trans/dram_trans_queue.cc
@@ -41,7 +41,7 @@ Status DramTransQueue::Setup(const int32_t deviceId, TaskSet* failureSet,
 }
 
 void DramTransQueue::Push(std::list<Task::Shard>& shards) noexcept {
-    this->backend_.Push(std::move(shards));
+    this->backend_.Push(std::list<Task::Shard>(std::make_move_iterator(shards.begin()), std::make_move_iterator(shards.end())));
 }
 
 bool DramTransQueue::Init(Device& device) {
