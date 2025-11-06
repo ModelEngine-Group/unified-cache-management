af31ee7e3eb710b7eff09f1bb3df6ad6d9c9bc0a
Mag1c.H <hemajun815@163.com>
2025-11-06 14:45:37 +0800

    pcstore

---
+++ ucm/store/pcstore/cc/domain/trans/trans_task.h
@@ -0,0 +1,80 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * */
+#ifndef UNIFIEDCACHE_TRANS_TASK_H
+#define UNIFIEDCACHE_TRANS_TASK_H
+
+#include <atomic>
+#include <limits>
+#include <list>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+namespace UC {
+
+class TransTask {
+    static size_t NextId() noexcept
+    {
+        static std::atomic<size_t> id{invalid + 1};
+        return id.fetch_add(1, std::memory_order_relaxed);
+    };
+
+public:
+    enum class Type { DUMP, LOAD };
+    struct Shard {
+        uintptr_t address;
+        size_t offset;
+        size_t length;
+        Shard(uintptr_t address, size_t offset, size_t length)
+            : address{address}, offset{offset}, length{length}
+        {
+        }
+    };
+    static constexpr auto invalid = std::numeric_limits<size_t>::min();
+    TransTask(Type&& type, std::string&& brief)
+        : id_{NextId()}, type_{std::move(type)}, brief_{std::move(brief)}
+    {
+    }
+    void Append(const std::string& block, const size_t offset, const uintptr_t address,
+                const size_t length)
+    {
+        auto& shard = shards_.emplace_back(address, offset, length);
+        addresses_.push_back(address);
+        grouped_[block].push_back(&shard);
+    }
+    uintptr_t* Addresses() { return addresses_.data(); }
+    size_t Number() const { return addresses_.size(); }
+
+private:
+    size_t id_;
+    Type type_;
+    std::string brief_;
+    std::list<Shard> shards_;
+    std::vector<uintptr_t> addresses_;
+    std::unordered_map<std::string, std::list<Shard*>> grouped_;
+};
+
+} // namespace UC
+
+#endif
