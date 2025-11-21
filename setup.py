6f12ea8f89b47695ab9064623dc9310990b15727
simshi <yuanmin.shi@metax-tech.com>
2025-11-21 06:28:43 +0000

    feat: add MetaX MACA device support

--- setup.py
+++ setup.py
@@ -50,6 +50,10 @@ def _is_musa() -> bool:
     return PLATFORM == "musa"
 
 
+def _is_maca() -> bool:
+    return PLATFORM == "maca"
+
+
 class CMakeExtension(Extension):
     def __init__(self, name: str, sourcedir: str = ""):
         super().__init__(name, sources=[])
@@ -91,6 +95,9 @@ class CMakeBuild(build_ext):
             cmake_args.append("-DRUNTIME_ENVIRONMENT=ascend")
         elif _is_musa():
             cmake_args.append("-DRUNTIME_ENVIRONMENT=musa")
+        elif _is_maca():
+            cmake_args.append("-DRUNTIME_ENVIRONMENT=maca")
+            cmake_args.append("-DBUILD_UCM_SPARSE=OFF")
         else:
             raise RuntimeError(
                 "No supported accelerator found. "
