21accb395b5d4594a69c32c22505e7e639b1ae57
zzycode1005 <2543624316@qq.com>
2025-11-27 17:39:10 -0800

    Remove Chinese comments

--- test/common/uc_eval/utils/client.py
+++ test/common/uc_eval/utils/client.py
@@ -399,7 +399,7 @@ class MultiDialogClient(BaseClient):
         turns = self._convert_conversation_2_turns(conversion, 2)
         for i, turn in enumerate(turns):
             in_content, reply = turn[0]["content"], turn[1]["content"]
-            # 更新payload，然后发送请求
+            # Update payload, then send request
             prompt = self._update_request_body(history, in_content)
             record: RequestRecord = self.send_request(prompt, max_tokens)
             record.case_name = case_name
