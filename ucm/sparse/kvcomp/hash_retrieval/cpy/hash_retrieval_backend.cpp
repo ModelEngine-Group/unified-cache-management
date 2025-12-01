36ced8babc4a7f2299f0bba53cf5e6d53f94666d
Mag1c.H <hemajun815@163.com>
2025-12-01 22:55:31 +0800

    fix codestyle

--- ucm/sparse/kvcomp/hash_retrieval/cpy/hash_retrieval_backend.cpp
+++ ucm/sparse/kvcomp/hash_retrieval/cpy/hash_retrieval_backend.cpp
@@ -1,11 +1,34 @@
-// hash_retrieval_backend.cpp
-
+/**
+ * MIT License
+ *
+ * Copyright (c) 2025 Huawei Technologies Co., Ltd. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ * */
 #include <algorithm>
 #include <atomic>
 #include <climits> // 用于UINT16_MAX
 #include <condition_variable>
 #include <cstddef>
 #include <cstdint>
+#include <cstring>
+#include <errno.h>
 #include <iostream>
 #include <mutex>
 #include <omp.h>
@@ -13,18 +36,16 @@
 #include <pybind11/pybind11.h>
 #include <queue>
 #include <random>
+#include <sched.h>
 #include <thread>
+#include <unistd.h>
 #include <unordered_map>
 #include <vector>
+
 #ifdef NUMA_ENABLED
 #include <numa.h>
 #include <numaif.h>
-#include <numa.h>
 #endif
-#include <cstring>
-#include <errno.h>
-#include <sched.h>
-#include <unistd.h>
 
 #if defined(__ARM_NEON) || defined(__ARM_NEON__)
 #include <arm_neon.h>
