0585ff66aac4157b5172f7def06942bfcecfe052
zbb200819 <1130072360@qq.com>
2025-12-01 05:57:38 -0800

    cleancode

--- ucm/sparse/gsa/offload_ops/src/select_topk_block.cpp
+++ ucm/sparse/gsa/offload_ops/src/select_topk_block.cpp
@@ -1,37 +1,31 @@
+#include "select_topk_block.h"
 #include <algorithm>
-#include <utility>
+#include <cmath>
 #include <limits>
 #include <stdexcept>
-#include <cmath>
-#include "select_topk_block.h"
+#include <utility>
 
 namespace SelectTopkBlock {
 #define OMP_THREAD_NUM 16u
 
-bool TopkBlockSelector::ValidateParameters(float* q, const float* kRepre,
-                                           uint32_t numBlock, uint32_t kHead, uint32_t qHead,
-                                           uint32_t numKrepre, uint32_t headSize)
+bool TopkBlockSelector::ValidateParameters(float* q, const float* kRepre, uint32_t numBlock,
+                                           uint32_t kHead, uint32_t qHead, uint32_t numKrepre,
+                                           uint32_t headSize)
 {
-    return (q != nullptr) && (kRepre != nullptr) && 
-           (numBlock > 0) && (kHead > 0) && (qHead > 0) &&
+    return (q != nullptr) && (kRepre != nullptr) && (numBlock > 0) && (kHead > 0) && (qHead > 0) &&
            (numKrepre > 0) && (headSize > 0);
 }
 
-void TopkBlockSelector::TopKImpl(const float* scores, uint32_t numScores, uint32_t k, int32_t* topkIndices)
+void TopkBlockSelector::TopKImpl(const float* scores, uint32_t numScores, uint32_t k,
+                                 int32_t* topkIndices)
 {
     if (startWindow_ + endWindow_ >= numScores || k >= numScores || k == 0) {
-        for (uint32_t i = 0; i < numScores; ++i) {
-            topkIndices[i] = i;
-        }
+        for (uint32_t i = 0; i < numScores; ++i) { topkIndices[i] = i; }
         return;
     }
     uint32_t idx = 0;
-    for (uint32_t i = 0; i < startWindow_; ++i) {
-        topkIndices[idx++] = i;
-    }
-    for (uint32_t i = 0; i < endWindow_; ++i) {
-        topkIndices[idx++] = numScores - endWindow_ + i;
-    }
+    for (uint32_t i = 0; i < startWindow_; ++i) { topkIndices[idx++] = i; }
+    for (uint32_t i = 0; i < endWindow_; ++i) { topkIndices[idx++] = numScores - endWindow_ + i; }
     int32_t midCount = k - startWindow_ - endWindow_;
     if (midCount > 0) {
         std::vector<uint32_t> middleIndices;
@@ -39,19 +33,16 @@ void TopkBlockSelector::TopKImpl(const float* scores, uint32_t numScores, uint32
         for (uint32_t i = startWindow_; i < numScores - endWindow_; ++i) {
             middleIndices.push_back(i);
         }
-        std::stable_sort(middleIndices.begin(), middleIndices.end(),
-                        [scores](uint32_t lhs, uint32_t rhs) {
-                            return scores[lhs] > scores[rhs];
-                        });
-        for (int32_t i = 0; i < midCount; ++i) {
-            topkIndices[idx++] = middleIndices[i];
-        }
+        std::stable_sort(
+            middleIndices.begin(), middleIndices.end(),
+            [scores](uint32_t lhs, uint32_t rhs) { return scores[lhs] > scores[rhs]; });
+        for (int32_t i = 0; i < midCount; ++i) { topkIndices[idx++] = middleIndices[i]; }
     }
 }
 
-float TopkBlockSelector::ComputeBlockScore(float* qMean, const float* blockBase,
-                                           uint32_t kHead, uint32_t numKrepre,
-                                           uint32_t headSize, const VecProductClass& vecProduct)
+float TopkBlockSelector::ComputeBlockScore(float* qMean, const float* blockBase, uint32_t kHead,
+                                           uint32_t numKrepre, uint32_t headSize,
+                                           const VecProductClass& vecProduct)
 {
     const size_t headOffset = headSize;
     const size_t normOffset = headSize;
@@ -80,8 +71,10 @@ const VecProductClass& TopkBlockSelector::ThreadLocalVecProduct::GetInstance()
     return instance;
 }
 
-std::vector<float> TopkBlockSelector::ComputeKQDotScores(const float* __restrict qMean, const float* __restrict kRepre,
-                                                         uint32_t numBlock, uint32_t kHead, uint32_t numKrepre, uint32_t headSize)
+std::vector<float> TopkBlockSelector::ComputeKQDotScores(const float* __restrict qMean,
+                                                         const float* __restrict kRepre,
+                                                         uint32_t numBlock, uint32_t kHead,
+                                                         uint32_t numKrepre, uint32_t headSize)
 {
     std::vector<float> blockScores(numBlock, 0.0f);
     const size_t blockOffset = static_cast<size_t>(kHead * numKrepre * headSize);
@@ -92,16 +85,16 @@ std::vector<float> TopkBlockSelector::ComputeKQDotScores(const float* __restrict
         if (idxBlock + 1 < numBlock) {
             __builtin_prefetch(kRepre + (idxBlock + 1) * blockOffset, 0, 1);
         }
-        blockScores[idxBlock] = ComputeBlockScore(const_cast<float*>(qMean), blockBase, kHead, numKrepre, headSize, vecProduct);
+        blockScores[idxBlock] = ComputeBlockScore(const_cast<float*>(qMean), blockBase, kHead,
+                                                  numKrepre, headSize, vecProduct);
     }
     return blockScores;
 }
 
-void TopkBlockSelector::ComputeQHeadMean(float* __restrict q, uint32_t kHead, uint32_t qHead, uint32_t headSize)
+void TopkBlockSelector::ComputeQHeadMean(float* __restrict q, uint32_t kHead, uint32_t qHead,
+                                         uint32_t headSize)
 {
-    if (kHead == qHead) {
-        return;
-    }
+    if (kHead == qHead) { return; }
     const VecProductClass& vecProduct = ThreadLocalVecProduct::GetInstance();
     const uint32_t groupSize = qHead / kHead;
     for (uint32_t kIdx = 0; kIdx < kHead; ++kIdx) {
@@ -112,28 +105,25 @@ void TopkBlockSelector::ComputeQHeadMean(float* __restrict q, uint32_t kHead, ui
     }
 }
 
-void TopkBlockSelector::SelectTopK(float* q, const float* kRepre,
-                                   uint32_t numBlock, uint32_t kHead, uint32_t qHead,
-                                   uint32_t numKrepre, uint32_t headSize,
+void TopkBlockSelector::SelectTopK(float* q, const float* kRepre, uint32_t numBlock, uint32_t kHead,
+                                   uint32_t qHead, uint32_t numKrepre, uint32_t headSize,
                                    uint32_t topkLength, int32_t* topkResult)
 {
     if (!ValidateParameters(q, kRepre, numBlock, kHead, qHead, numKrepre, headSize) ||
         topkResult == nullptr || topkLength == 0) {
-        return;        
+        return;
     }
     ComputeQHeadMean(q, kHead, qHead, headSize);
-    const std::vector<float> scores = ComputeKQDotScores(q, kRepre, numBlock,
-                                                         kHead, numKrepre, headSize);
+    const std::vector<float> scores =
+        ComputeKQDotScores(q, kRepre, numBlock, kHead, numKrepre, headSize);
     TopKImpl(scores.data(), numBlock, topkLength, topkResult);
 }
 
 void TopkBlockSelector::SelectTopKBS(const std::vector<float*>& qCacheVec,
                                      const std::vector<const float*>& kfCacheVec,
-                                     const std::vector<int32_t*>& topkCacheVec,
-                                     uint32_t numBatch,
-                                     const std::vector<uint32_t>& numBlockVec,
-                                     uint32_t kHead, uint32_t qHead,
-                                     uint32_t numKrepre, uint32_t headSize,
+                                     const std::vector<int32_t*>& topkCacheVec, uint32_t numBatch,
+                                     const std::vector<uint32_t>& numBlockVec, uint32_t kHead,
+                                     uint32_t qHead, uint32_t numKrepre, uint32_t headSize,
                                      const std::vector<uint32_t>& topkLengthVec)
 {
     for (uint32_t bs = 0; bs < numBatch; ++bs) {
@@ -142,9 +132,8 @@ void TopkBlockSelector::SelectTopKBS(const std::vector<float*>& qCacheVec,
         float* q = qCacheVec[bs];
         const float* kRepre = kfCacheVec[bs];
         int32_t* topkResult = topkCacheVec[bs];
-        SelectTopK(q, kRepre, numBlock, kHead, qHead,
-                   numKrepre, headSize, topkLength, topkResult);
+        SelectTopK(q, kRepre, numBlock, kHead, qHead, numKrepre, headSize, topkLength, topkResult);
     }
 }
 
-}
+} // namespace SelectTopkBlock
--- ucm/sparse/gsa/prefetch/include/kvcache_pre.h
+++ ucm/sparse/gsa/prefetch/include/kvcache_pre.h
@@ -1,199 +1,179 @@
 #ifndef ATB_KV_CACHE_PRE_H
 #define ATB_KV_CACHE_PRE_H
-#include <string>
-#include <vector>
-#include <unordered_map>
-#include <iostream>
-#include <queue>
-#include <thread>
-#include <mutex>
-#include <condition_variable>
-#include <future>
-#include <functional>
 #include <atomic>
-#include <pybind11/numpy.h>
 #include <chrono>
-#include <stdio.h>
-#include <stdarg.h>
-#include <torch/torch.h>
+#include <condition_variable>
 #include <cstring>
-#include <omp.h>
-#include <unordered_set>
+#include <functional>
+#include <future>
+#include <iostream>
 #include <kvcache_log.h>
-#include <sstream>
 #include <map>
+#include <mutex>
+#include <omp.h>
+#include <pybind11/numpy.h>
+#include <queue>
+#include <sstream>
+#include <stdarg.h>
 #include <stdexcept>
+#include <stdio.h>
+#include <string>
+#include <thread>
+#include <torch/torch.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
 #include "../../../../store/ucmstore.h"
 
 namespace py = pybind11;
 
-namespace ucmprefetch
-{
-    typedef struct {
-        int topkLen;
-        std::string reqID;
-        int layerID;
-        int topkIndex;
-        int bsIndex;
-    } PrefetchReqInfo;
+namespace ucmprefetch {
+typedef struct {
+    int topkLen;
+    std::string reqID;
+    int layerID;
+    int topkIndex;
+    int bsIndex;
+} PrefetchReqInfo;
 
-    class ThreadPool
+class ThreadPool {
+public:
+    static ThreadPool* GetInst()
     {
-    public:
-        static ThreadPool *GetInst()
-        {
-            static ThreadPool pool(1);
-            return &pool;
-        }
+        static ThreadPool pool(1);
+        return &pool;
+    }
 
-        ~ThreadPool();
+    ~ThreadPool();
 
-        template<class F, class ... Args>
-        auto enqueue(F&& f, Args&&... args) -> std::future<typename std::result_of<F(Args...)>::type>;
+    template <class F, class... Args>
+    auto enqueue(F&& f, Args&&... args) -> std::future<typename std::result_of<F(Args...)>::type>;
 
-        size_t GetActiveThreads() const;
+    size_t GetActiveThreads() const;
 
-    private:
-        explicit ThreadPool(size_t threadCount);
-        std::vector<std::thread> workers;
-        std::queue<std::function<void()>> tasks;
-        mutable std::mutex queueMutex;
-        bool stop;
-        std::condition_variable condition;
-        std::atomic<size_t> activeThreads{0};
-        size_t maxThreads;
-    };
+private:
+    explicit ThreadPool(size_t threadCount);
+    std::vector<std::thread> workers;
+    std::queue<std::function<void()>> tasks;
+    mutable std::mutex queueMutex;
+    bool stop;
+    std::condition_variable condition;
+    std::atomic<size_t> activeThreads{0};
+    size_t maxThreads;
+};
 
-    void MutliBSThreadFun(void *args);
+void MutliBSThreadFun(void* args);
 
-    class __attribute__((visibility("hidden"))) GSAPrefetchEngineC
-    {
-    private:
-        std::map<std::string, std::vector<std::map<int, int>>> mDocsTables;
-        std::map<std::string, std::vector<std::map<int, int>>> mBlocksMap;
-        torch::Tensor mLoadSuccessBlocks;
-        torch::Tensor mFreeBlock;
-        torch::Tensor mFreeBlockLen;
-        torch::Tensor mSuccessTableLen;
-        torch::Tensor mUseTopkIdxs;
-        int mLayerNum;
-        int mRank = -1;
-        uint32_t mMaxBs = 30;
-        std::vector<std::string> mReqIdList;
-        int *mTopkLenList = NULL;
-        int *mBsIndexList = NULL;
-        uint32_t runBsLen = 0;
-        bool mIsLog = false;
-        bool mIsPrefetchDone = true;
-        bool mUseMla = false;
-        Logger mLogger;
-        ThreadPool *mThreadPool;
-        uint32_t mDecodeStep = 0;
-        uint32_t mMaxTopkLen = 0;
-        uint32_t mMaxBlocksLen = 0;
-        std::unordered_set<std::string> mDelSeqIds;
-        std::map<std::string, std::vector<std::vector<int>>> allNeedLoadBlock;
-        std::map<std::string, std::vector<std::vector<int>>> allMissIdxs;
-        std::map<std::string, int> mPromptLen;
-        UC::CCStore<> *mStore = nullptr;
-        std::vector<torch::Tensor> mKvCaches;
-        uint32_t mBlockSize = 128;
-        uint32_t mTensorElemSize = 2; // fp16
-        uint32_t mHeadNum = 40;
-        uint32_t mHeadSzie = 128;
-        uint32_t mTPSize = 2;
-        std::map<std::string, std::vector<std::string>> mAllBlcoksHash;
-        uint32_t mKVSzieBytes = 0;
-        uint32_t mExtraTopkLen = 16;
-        bool mIsPythonLoad = false;
-    public:
-        std::mutex mMutex;
-        bool mStopPrefetch = false;
+class __attribute__((visibility("hidden"))) GSAPrefetchEngineC {
+private:
+    std::map<std::string, std::vector<std::map<int, int>>> mDocsTables;
+    std::map<std::string, std::vector<std::map<int, int>>> mBlocksMap;
+    torch::Tensor mLoadSuccessBlocks;
+    torch::Tensor mFreeBlock;
+    torch::Tensor mFreeBlockLen;
+    torch::Tensor mSuccessTableLen;
+    torch::Tensor mUseTopkIdxs;
+    int mLayerNum;
+    int mRank = -1;
+    uint32_t mMaxBs = 30;
+    std::vector<std::string> mReqIdList;
+    int* mTopkLenList = NULL;
+    int* mBsIndexList = NULL;
+    uint32_t runBsLen = 0;
+    bool mIsLog = false;
+    bool mIsPrefetchDone = true;
+    bool mUseMla = false;
+    Logger mLogger;
+    ThreadPool* mThreadPool;
+    uint32_t mDecodeStep = 0;
+    uint32_t mMaxTopkLen = 0;
+    uint32_t mMaxBlocksLen = 0;
+    std::unordered_set<std::string> mDelSeqIds;
+    std::map<std::string, std::vector<std::vector<int>>> allNeedLoadBlock;
+    std::map<std::string, std::vector<std::vector<int>>> allMissIdxs;
+    std::map<std::string, int> mPromptLen;
+    UC::CCStore<>* mStore = nullptr;
+    std::vector<torch::Tensor> mKvCaches;
+    uint32_t mBlockSize = 128;
+    uint32_t mTensorElemSize = 2; // fp16
+    uint32_t mHeadNum = 40;
+    uint32_t mHeadSzie = 128;
+    uint32_t mTPSize = 2;
+    std::map<std::string, std::vector<std::string>> mAllBlcoksHash;
+    uint32_t mKVSzieBytes = 0;
+    uint32_t mExtraTopkLen = 16;
+    bool mIsPythonLoad = false;
 
-    private:
-        void LoadKVToHBM(std::vector<int> loadNPUBlockIDs,
-            std::vector<int> missIdxs, int layerID, std::string reqID);
+public:
+    std::mutex mMutex;
+    bool mStopPrefetch = false;
 
-        void GetHitAndMissBlock(PrefetchReqInfo oneBsInfo,
-            std::unordered_set<int> &hitBlocks,
-            std::map<int, int> &hitBlocksIdx,
-            std::vector<int> &missIdxs);
+private:
+    void LoadKVToHBM(std::vector<int> loadNPUBlockIDs, std::vector<int> missIdxs, int layerID,
+                     std::string reqID);
 
-        void RunPrefetchH2D(PrefetchReqInfo oneBsInfo,
-            std::unordered_set<int> &hitBlocks,
-            std::map<int, int> &hitBlocksIdx,
-            std::vector<int> &missIdxs);
+    void GetHitAndMissBlock(PrefetchReqInfo oneBsInfo, std::unordered_set<int>& hitBlocks,
+                            std::map<int, int>& hitBlocksIdx, std::vector<int>& missIdxs);
 
-        void RunOneBsPrefetch(std::string reqID, int topkLen,
-            int bsIndex, int topkIndex);
+    void RunPrefetchH2D(PrefetchReqInfo oneBsInfo, std::unordered_set<int>& hitBlocks,
+                        std::map<int, int>& hitBlocksIdx, std::vector<int>& missIdxs);
 
-    public:
-        ~GSAPrefetchEngineC();
+    void RunOneBsPrefetch(std::string reqID, int topkLen, int bsIndex, int topkIndex);
 
-        GSAPrefetchEngineC(torch::Tensor &freeBlock,
-            torch::Tensor &loadSuccessBlocks,
-            torch::Tensor &freeBlockLen,
-            torch::Tensor &successTableLen,
-            std::vector<uint32_t> &kvShape,
-            bool useMla,
-            bool isLog,
-            int tpSize,
-            int rank,
-            int extraTopkLen,
-            bool isPythonLoad
-        );
+public:
+    ~GSAPrefetchEngineC();
 
-        void SetBlocksMap(std::string reqID, std::vector<int> &blockTableList,
-            std::vector<int> &selectIndex, std::vector<std::string> &blocksHash,
-            int maxIdx);
+    GSAPrefetchEngineC(torch::Tensor& freeBlock, torch::Tensor& loadSuccessBlocks,
+                       torch::Tensor& freeBlockLen, torch::Tensor& successTableLen,
+                       std::vector<uint32_t>& kvShape, bool useMla, bool isLog, int tpSize,
+                       int rank, int extraTopkLen, bool isPythonLoad);
 
-        void SetBlocksMapMultiLayer(std::string reqID,
-            std::vector<std::map<int, int>> &remainMap,
-            std::vector<std::map<int, int>> &prefetchMap,
-            std::vector<std::string> &blocksHash,
-            int maxIdx);
+    void SetBlocksMap(std::string reqID, std::vector<int>& blockTableList,
+                      std::vector<int>& selectIndex, std::vector<std::string>& blocksHash,
+                      int maxIdx);
 
-        void CheckInputIndex(uint32_t maxLen, uint32_t index);
+    void SetBlocksMapMultiLayer(std::string reqID, std::vector<std::map<int, int>>& remainMap,
+                                std::vector<std::map<int, int>>& prefetchMap,
+                                std::vector<std::string>& blocksHash, int maxIdx);
 
-        void AddBlocksMap(std::string reqID, int idx, int blockID);
+    void CheckInputIndex(uint32_t maxLen, uint32_t index);
 
-        void DelBlocksMap(std::string reqID);
+    void AddBlocksMap(std::string reqID, int idx, int blockID);
 
-        void DelReqIDRun();
+    void DelBlocksMap(std::string reqID);
 
-        void SetBlockTableInfo(torch::Tensor &blockTables,
-            torch::Tensor &blockLengths,
-            torch::Tensor &inputTopkBuf, int step);
+    void DelReqIDRun();
 
-        void RunAsyncPrefetchBs(std::vector<std::string> &reqIDsInput,
-            std::vector<int> &topkLensInput,
-            std::vector<int> &bsIndexInput,
-            std::vector<torch::Tensor> &kvCaches,
-            void *storePtr);
+    void SetBlockTableInfo(torch::Tensor& blockTables, torch::Tensor& blockLengths,
+                           torch::Tensor& inputTopkBuf, int step);
 
-        int CallPrefetchProcessFun();
+    void RunAsyncPrefetchBs(std::vector<std::string>& reqIDsInput, std::vector<int>& topkLensInput,
+                            std::vector<int>& bsIndexInput, std::vector<torch::Tensor>& kvCaches,
+                            void* storePtr);
 
-        void PrintMap(std::string reqID, int i);
+    int CallPrefetchProcessFun();
 
-        bool GetPrefetchStatus();
+    void PrintMap(std::string reqID, int i);
 
-        void SetPrefetchStatus(bool flag);
+    bool GetPrefetchStatus();
 
-        void SetModelRunningStatus(bool flag);
+    void SetPrefetchStatus(bool flag);
 
-        size_t GetOffset(uint32_t layerID, bool isV);
+    void SetModelRunningStatus(bool flag);
 
-        size_t GetOffsetNew(uint32_t layerID, bool isV);
+    size_t GetOffset(uint32_t layerID, bool isV);
 
-        std::map<std::string, std::vector<std::vector<int>>> ObtainLoadBlocks();
+    size_t GetOffsetNew(uint32_t layerID, bool isV);
 
-        std::map<std::string, std::vector<std::vector<int>>> ObtainMissIdxs();
+    std::map<std::string, std::vector<std::vector<int>>> ObtainLoadBlocks();
 
-        std::map<std::string, std::vector<std::map<int, int>>> ObtainBlocksMap();
+    std::map<std::string, std::vector<std::vector<int>>> ObtainMissIdxs();
 
-        std::map<std::string, std::vector<std::map<int, int>>> ObtainDocsMap();
-    };
+    std::map<std::string, std::vector<std::map<int, int>>> ObtainBlocksMap();
 
-} // namespace uc
+    std::map<std::string, std::vector<std::map<int, int>>> ObtainDocsMap();
+};
+
+} // namespace ucmprefetch
 
 #endif
