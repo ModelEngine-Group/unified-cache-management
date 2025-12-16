c95dc1acd615a188cee9a8f842241d0ce2f5fed9
ldeng <ldeng.sjtu@gmail.com>
2025-12-16 21:37:07 +0800

    [Feature] Enable KVComp and update GSAConfig parameters
    Set CUDA_TOPK to True and introduced new parameters for KVComp, including ENABLE_KVCOMP, KVCOMP_PRESERVE_BLOCKS, and RECENT_WINDOW_SZ in the GSAConfig class. Updated compute_topk_len method to utilize KVComp settings for top-k length computation.

--- ucm/sparse/utils.py
+++ ucm/sparse/utils.py
@@ -7,18 +7,25 @@ MIN_TOPK_LEN = 32
 MAX_TOPK_LEN = 48
 MAX_BS = 256
 SEG_PREFILL_THRESHOLD = 8400
-CUDA_TOPK = False
+CUDA_TOPK = True #True for KVComp
 PTOPK_PREFETCH_ENABLE = False
 VLLM_CUDA_MEM_ALIGN_KV_CACHE = False
 INIT_WINDOW_SZ = 1
 NUM_PREFETCH_BLOCKS = 1
 NUM_GSA_BLOCKS = 1
 
+#KVComp utils
+ENABLE_KVCOMP = True        #should be True for KVComp inside GSA
+KVCOMP_PRESERVE_BLOCKS = 32 #number of blocks to preserve in KVComp for attention computation
+RECENT_WINDOW_SZ = 4
+
 
 class GSAConfig:
     def __init__(self):
         self.block_size = DEFAULT_BLOCK_SIZE
         self.init_windows_size = INIT_WINDOW_SZ
+        self.recent_window_size = RECENT_WINDOW_SZ
+        self.kvcomp_preserve_blocks = KVCOMP_PRESERVE_BLOCKS
         self.num_prefetch_blocks = NUM_PREFETCH_BLOCKS
         self.min_topk_len = MIN_TOPK_LEN
         self.max_topk_len = MAX_TOPK_LEN
@@ -33,12 +40,22 @@ class GSAConfig:
         self.init_windows_size = math.ceil(
             INIT_WINDOW_SZ * DEFAULT_BLOCK_SIZE / block_szie
         )
+        self.recent_window_size = math.ceil(
+            RECENT_WINDOW_SZ * DEFAULT_BLOCK_SIZE / block_szie
+        )
+        self.kvcomp_preserve_blocks = math.ceil(
+            KVCOMP_PRESERVE_BLOCKS * DEFAULT_BLOCK_SIZE / block_szie
+        )
         self.num_gsa_blocks = math.ceil(
             NUM_GSA_BLOCKS * DEFAULT_BLOCK_SIZE / block_szie
         )
 
+    # (NOTE) raw_seq_len is the number of blocks (not tokens) in the sequence)
     def compute_topk_len(self, raw_seq_len):
-        topk_len = math.ceil(raw_seq_len * 0.3)
+        if ENABLE_KVCOMP:
+            topk_len = self.kvcomp_preserve_blocks
+        else:
+            topk_len = math.ceil(raw_seq_len * 0.3)
         # topk_len = max(1, topk_len)
         if topk_len < self.min_topk_len:
             topk_len = min(self.min_topk_len, raw_seq_len)
