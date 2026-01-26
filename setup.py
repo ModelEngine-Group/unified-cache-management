a707e09cfe0bdaf8c67299b198296ecc3881997f
flesher0813 <33923823+flesher0813@users.noreply.github.com>
2026-01-26 11:46:50 +0800

    release 0.3.0 (#677)
    <!--  Thanks for sending a pull request!
    
    BEFORE SUBMITTING, PLEASE READ OUR OFFICIAL WEBSITE.
    
    -->
    
    # Purpose
    
    What this PR does / why we need it?
    <!--
    - Please clarify what changes you are proposing. The purpose of this
    section is to outline the changes and how this PR fixes the issue.
    If possible, please consider writing useful notes for better and faster
    reviews in your PR.
    
    - Please clarify why the changes are needed. For instance, the use case
    and bug description.
    
    - Fixes #
    -->
    release 0.3.0
    # Modifications 
    
    Does this PR introduce _any_ user-facing change?
    <!--
    Note that it means *any* user-facing change including all aspects such
    as API, interface or other behavior changes.
    Documentation-only updates are not considered user-facing changes.
    -->
    release 0.3.0
    # Test
    
    How was this patch tested?
    <!--
    CI passed with new added/existing test.
    If it was tested in a way different from regular unit tests, please
    clarify how you tested step by step, ideally copy and paste-able, so
    that other reviewers can test and check, and descendants can verify in
    the future.
    If tests were not added, please describe why they were not added and/or
    why it was difficult to add.
    -->

--- setup.py
+++ setup.py
@@ -139,7 +139,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name="uc-manager",
-    version="0.2.0",
+    version="0.3.0",
     description="Unified Cache Management",
     author="Unified Cache Team",
     packages=find_packages(),
