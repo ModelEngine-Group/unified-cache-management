ea816302ebb75981e3a4aef7844466ce77afa3b7
harrisonyhq <harrisonyhq@gmail.com>
2026-01-28 16:31:27 +0800

    [bugfix] Fix compilation error due to missing atomic include (#693)
    ## Purpose
    Fix compilation error due to missing atomic include
    ## Modifications 
    Add #include <atomic>
    ## Test

--- ucm/store/empty/cpy/empty_store.py.cc
+++ ucm/store/empty/cpy/empty_store.py.cc
@@ -21,6 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  * */
+#include <atomic>
 #include "template/store_binder.h"
 #include "ucmstore_v1.h"
 
