029c4047294f89eceeb0790fe3a36a0d63aceac2
ayaka836 <nieteng781@gmail.com>
2026-03-09 05:43:18 -0700

    [Fix] change package 'wrapt' version

--- setup.py
+++ setup.py
@@ -198,7 +198,7 @@ setup(
     packages=find_packages() + [""],
     package_dir={"": "."},
     python_requires=">=3.10",
-    install_requires="wrapt>=2.1.0",
+    install_requires="wrapt>=2.0.1",
     ext_modules=[CMakeExtension(name="ucm", source_dir=ROOT_DIR)],
     cmdclass={"build_ext": CMakeBuild},
     zip_safe=False,
