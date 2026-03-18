3632f25a068ce6fbaaea3b840ec444a7a873a886
lijiachen <Jiachen1018@outlook.com>
2026-03-18 05:37:00 -0700

    rate limit

--- ucm/shared/infra/logger/cpy/spdlog_logger.py.cc
+++ ucm/shared/infra/logger/cpy/spdlog_logger.py.cc
@@ -32,11 +32,17 @@ void LogWrapper(Level lv, std::string file, std::string func, int line, std::str
     Log(std::move(lv), std::move(file), std::move(func), line, std::move(msg));
 }
 
+void RateLimitLogWrapper(Level lv, std::string file, std::string func, int line, std::string msg)
+{
+    LogRateLimit(std::move(lv), std::move(file), std::move(func), line, std::move(msg));
+}
+
 PYBIND11_MODULE(ucmlogger, m)
 {
     m.def("setup", &Setup);
     m.def("flush", &Flush);
     m.def("log", &LogWrapper);
+    m.def("log_rate_limit", &RateLimitLogWrapper);
     m.def("isEnabledFor", &isEnabledFor);
     py::enum_<Level>(m, "Level")
         .value("DEBUG", Level::DEBUG)
