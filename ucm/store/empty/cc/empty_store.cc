92f411984e738bd39ab6e8420f8ff297923264cf
Mag1c.H <hemajun815@163.com>
2026-03-25 15:06:34 +0800

    [opt] stack-protector on EmptyStore (#866)
    ## Purpose
    stack-protector on EmptyStore
    ## Test
    ```
    readelf -Ws libemptystore.so | grep __stack_chk_fail
         7: 0000000000000000     0 FUNC    GLOBAL DEFAULT  UND __stack_chk_fail@GLIBC_2.4 (6)
    ```

--- ucm/store/empty/cc/empty_store.cc
+++ ucm/store/empty/cc/empty_store.cc
@@ -26,13 +26,15 @@
 
 namespace UC::EmptyStore {
 
+std::vector<uint8_t> OnLookup(size_t num) { return std::vector<uint8_t>(num, false); }
+
 class EmptyStore : public StoreV1 {
 public:
     Status Setup(const Detail::Dictionary& config) { return Status::OK(); }
     std::string Readme() const { return "EmptyStore"; }
     Expected<std::vector<uint8_t>> Lookup(const Detail::BlockId* blocks, size_t num)
     {
-        return std::vector<uint8_t>(num, false);
+        return OnLookup(num);
     }
     Expected<ssize_t> LookupOnPrefix(const Detail::BlockId* blocks, size_t num) { return -1; }
     void Prefetch(const Detail::BlockId* blocks, size_t num) {}
