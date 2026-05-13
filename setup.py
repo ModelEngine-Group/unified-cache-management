6e7231e25722e4a7908645997b96cba7de199618
黄可欣 <2218887102@qq.com>
2026-05-13 09:54:40 +0800

    修改setup新增行格式

--- setup.py
+++ setup.py
@@ -34,12 +34,7 @@ ROOT_DIR = os.path.abspath(os.path.dirname(__file__))
 PLATFORM = os.getenv("PLATFORM")
 ENABLE_SPARSE = os.getenv("ENABLE_SPARSE")
 ENABLE_MINDIE = os.getenv("UCM_ENABLE_MINDIE", "0") not in ("", "0", "false", "False")
-ENABLE_GDR_STREAM = os.getenv("UCM_ENABLE_GDR_STREAM", "0") not in (
-    "",
-    "0",
-    "false",
-    "False",
-)
+ENABLE_GDR_STREAM = os.getenv("UCM_ENABLE_GDR_STREAM", "0") not in ("", "0", "false", "False")
 
 
 def get_abi_flag_from_env() -> str:
