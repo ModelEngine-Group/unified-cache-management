e84b9f306f3c9fce59f62403cd099f4cec819951
wenxinwang <wangwenxin21@huawei.com>
2026-05-14 23:39:29 -0700

    fix

--- ucm/integration/vllm/ucm_connector.py
+++ ucm/integration/vllm/ucm_connector.py
@@ -2655,8 +2655,8 @@ class UCMConnector(KVConnectorBase_V1, SupportsHMA):
             self.connector = UCMHybridLinearAttentionConnector(
                 vllm_config, role, kv_cache_config
             )
-        elif use_hma:
-            self.connector = UCMHMAConnector(vllm_config, role, kv_cache_config)
+        # elif use_hma:
+        #     self.connector = UCMHMAConnector(vllm_config, role, kv_cache_config)
         else:
             self.connector = UCMDirectConnector(vllm_config, role, kv_cache_config)
 
