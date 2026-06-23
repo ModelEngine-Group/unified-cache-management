85ebf96c4f83538b2711e4e0de447604f430cd59
qyh <qiuyuhao1@huawei.com>
2026-06-23 20:47:15 +0800

    fix double free

--- ucm/integration/vllm/patch/v0202/vllm/load_failure_patch.py
+++ ucm/integration/vllm/patch/v0202/vllm/load_failure_patch.py
@@ -4,6 +4,21 @@ from ucm.logger import init_logger
 logger = init_logger(__name__)
 
 
+# UCM double-free guard patch start
+@when_imported("vllm.v1.core.block_pool")
+def patch_core_block_pool(mod):
+    logger.debug(f"Patched {mod} called")
+
+    from ucm.integration.vllm.patch.v0202.vllm.v1.core import block_pool
+
+    patch_or_inject(
+        mod.BlockPool,
+        "free_blocks",
+        block_pool.BlockPool.free_blocks,
+    )
+# UCM double-free guard patch end
+
+
 @when_imported("vllm.v1.core.sched.scheduler")
 def patch_core_sched_scheduler(mod):
     logger.debug(f"Patched {mod} called")
