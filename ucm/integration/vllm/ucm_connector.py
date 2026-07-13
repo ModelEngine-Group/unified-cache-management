fb3dc263444ad4e3f98ed938318cfabb5b097ca0
pyxyzc <pyxyzc@gmail.com>
2026-07-13 16:35:45 +0800

    Rename asu_tensor_layout to tensor_layout

--- ucm/integration/vllm/ucm_connector.py
+++ ucm/integration/vllm/ucm_connector.py
@@ -337,7 +337,7 @@ class UCMDirectConnector(KVConnectorBase_V1):
             backends = [path for path in config["storage_backends"].split(":")]
             config["storage_backends"] = backends
         config["unique_id"] = f"{self.engine_id}"
-        config["asu_tensor_layout"] = "mla" if self.is_mla else "gqa"
+        config["tensor_layout"] = "mla" if self.is_mla else "gqa"
         if self._role == KVConnectorRole.WORKER:
             config["device_id"] = self.local_rank
             config["tensor_size_list"] = (
