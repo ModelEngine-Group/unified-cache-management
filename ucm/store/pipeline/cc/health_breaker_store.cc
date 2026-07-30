d3ea97bff7c3ae70255bc603e77d5bcdf71dcfaf
Yanzhao Wang <wyz159753@gmail.com>
2026-07-30 14:42:52 +0800

    [Feat] Stagger initial store health probes

--- ucm/store/pipeline/cc/health_breaker_store.cc
+++ ucm/store/pipeline/cc/health_breaker_store.cc
@@ -22,11 +22,24 @@
  * SOFTWARE.
  */
 #include "health_breaker_store.h"
+#include <random>
 #include "logger/logger.h"
 #include "metrics_api.h"
 #include "thread/cpu_affinity.h"
 
 namespace UC::PipelineStore {
+
+namespace {
+
+std::chrono::milliseconds RandomInitialProbeDelay(std::chrono::milliseconds interval)
+{
+    thread_local std::mt19937 generator{std::random_device{}()};
+    std::uniform_int_distribution<std::chrono::milliseconds::rep> distribution{interval.count() / 2,
+                                                                               interval.count()};
+    return std::chrono::milliseconds{distribution(generator)};
+}
+
+}  // namespace
 
 HealthBreakerStore::HealthBreakerStore(StoreV1* store, std::string storeId,
                                        HealthBreakerConfig config)
@@ -213,7 +226,7 @@ void HealthBreakerStore::ProbeLoop()
         UC_WARN("Failed({}) to set UCM health monitor thread name.", nameStatus);
     }
     std::unique_lock<std::mutex> lock(stopMutex_);
-    auto delay = config_.healthCheckInterval;
+    auto delay = RandomInitialProbeDelay(config_.healthCheckInterval);
     while (!stopCv_.wait_for(lock, delay, [this] { return stop_; })) {
         lock.unlock();
         const auto start = std::chrono::steady_clock::now();
